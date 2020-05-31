853b155897cfa115c39550a64beb2a275b592520
wanhar <wanhar@sehatq.com>
2020-05-31 20:37:20 +0700

    Initial Commit

---
+++ simple login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simple login
+//
+//  Created by Wanhar on 31/05/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
